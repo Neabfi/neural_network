1e3c8478155c78b9cd7c518d91c369e8e07d3952
Neabfi <fabien.delattre@outlook.com>
2016-06-16 20:33:37 +0200

    Initial commit

---
+++ build/lib/neural_network/Neuron.py
@@ -0,0 +1,19 @@
+import random
+import math
+
+class Neuron:
+    def __init__(self, numOutputs, index, inherit=None):
+        self.index = index
+        self.value = 0
+        self.outputWeights = []
+        for i in range(numOutputs):
+            self.outputWeights.append(random.random()*2-1 if not inherit else inherit[i])
+
+    def feedForward(self, prevLayer):
+        sum = 0.0
+        for i in range(len(prevLayer)):
+            sum += prevLayer[i].value * prevLayer[i].outputWeights[self.index]
+            self.value = self.activationFunction(sum)
+
+    def activationFunction(self, sum):
+        return math.tanh(sum)
---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "neural_network",
+    version = "0.1",
+    author = "Fabien Delattre",
+    author_email = "job@fabiendelattre.com",
+    description = ("Just a simple Neural Network."),
+    license = "MIT",
+    keywords = "neural network",
+    packages=['neural_network'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python"
+    ],
+)
