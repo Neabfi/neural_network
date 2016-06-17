26d70ff257865155bfba7ad7ebdc1557a42c7ffc
Neabfi <fabien.delattre@outlook.com>
2016-06-17 11:40:46 +0200

    Fix test syntax

--- tests/test_net.py
+++ tests/test_net.py
@@ -1,12 +1,12 @@
 from neural_network import *
 
 def test_net():
-    setInputsRange(0, 100):
-    setOutputsRange(0, 100):
+    setInputsRange(0, 100)
+    setOutputsRange(0, 100)
     net = Net([3, 2, 1, 2, 3])
     print(net)
     net.feedForward([50, 0, 80])
 
-    setInputsRange(0, 100, [0]):
-    setOutputsRange(0, 100, [0]):
+    setInputsRange(0, 100, [0])
+    setOutputsRange(0, 100, [0])
     net = Net([50, 0.2])
