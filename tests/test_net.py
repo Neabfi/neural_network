2eef2a0dfd4d5d6d952bf306f8695d80fa6e4b64
Neabfi <fabien.delattre@outlook.com>
2016-06-17 11:29:19 +0200

    Improve test

--- tests/test_net.py
+++ tests/test_net.py
@@ -1,5 +1,12 @@
 from neural_network import *
 
 def test_net():
+    setInputsRange(0, 100):
+    setOutputsRange(0, 100):
     net = Net([3, 2, 1, 2, 3])
-    net.feedForward([0.2, 0.8, 0.4])
+    print(net)
+    net.feedForward([50, 0, 80])
+
+    setInputsRange(0, 100, [0]):
+    setOutputsRange(0, 100, [0]):
+    net = Net([50, 0.2])
