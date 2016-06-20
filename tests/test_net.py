e20ce834640f5d241da92cc50cf83aa4b1fc73c2
Neabfi <fabien.delattre@outlook.com>
2016-06-20 17:47:58 +0200

    Update test

--- tests/test_net.py
+++ tests/test_net.py
@@ -1,13 +1,13 @@
 from neural_network import *
 
 def test_net():
-    net1 = Net([3, 2, 1, 2, 3])
+    net1 = Net(3, 2, 1, 2, 3)
     net1.setInputsRange(0, 100)
     net1.setOutputsRange(0, 100)
     print(net1)
     net1.feedForward([50, 0, 80])
 
-    net2 = Net([2, 2])
+    net2 = Net(2, 2)
     net2.setInputsRange(0, 100, [0])
     net2.setOutputsRange(0, 100, [0])
     print(net2)
@@ -16,7 +16,7 @@ def test_net():
 def test_geneticNet():
     nets = []
     for i in range(10):
-        nets.append(Net([2, 3, 4]))
+        nets.append(Net(2, 3, 4))
     genetic = GeneticNet(nets)
     genetic.reproduction()
     genetic.selection = 'wheel'
